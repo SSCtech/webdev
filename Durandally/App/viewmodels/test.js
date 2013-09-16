dad0d970f3193d2b9590f762ed21495f44dc408f
SSCtech <SSCtech@live.com>
2013-09-16 12:08:28 -0500

    first commit

---
+++ Durandally/App/viewmodels/test.js
@@ -0,0 +1,17 @@
+﻿define(function ()
+{
+  var vm =
+    {
+      MktsAndFacs: ko.observableArray(
+        [
+          { Market: "CHI", Facilities: ko.observableArray(["820", "840", "860"]) },
+          { Market: "PHX", Facilities: ko.observableArray(["943", "965", "987"]) },
+          { Market: "SAN", Facilities: ko.observableArray(["MW-C", "XY-Z", "AB-C"]) }
+        ]),
+      selectedMarket: ko.observable(),
+      selectedFacility: ko.observable()
+    };
+
+  return vm;
+});
+
---
+++ Durandally/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace Durandally.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
